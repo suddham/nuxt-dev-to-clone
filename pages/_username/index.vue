e60cf18d80d3413baaf8753b789144e6c459a080
Suddha Mukhopadhyay <suddha@bitcoin.com.au>
2020-06-01 14:29:54 +0530

    Add dev.to api pull Add card block with rising articles

--- pages/_username/index.vue
+++ pages/_username/index.vue
@@ -1,13 +1,7 @@
-<template>
-
-</template>
+<template> </template>
 
 <script>
-	export default {
-		name: 'index',
-	}
+export default {};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,36 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-dev-to-clone
-      </h1>
-      <h2 class="subtitle">
-        dev.to clone
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="page-wrapper">
+    <div class="article-cards-wrapper">
+      <article-card-block
+        v-for="(article, index) in articles"
+        :key="article.id"
+        :article="article"
+        class="article-card-block"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import ArticleCardBlock from "~/components/blocks/ArticleCardBlock";
 export default {
   components: {
-    Logo
+    ArticleCardBlock
+  },
+  data() {
+    return {
+      currentPage: 1,
+      articles: []
+    };
+  },
+  async fetch() {
+    const articles = await fetch(
+      `https://dev.to/api/articles?tag=nuxt&state=rising&page=${this.currentPage}`
+    ).then(res => res.json());
+
+    this.articles = this.articles.concat(articles);
   }
-}
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style scoped></style>
