f1fc0837bb76544dfde8fa9fe9777cb547dada50
Suddha Mukhopadhyay <suddha@bitcoin.com.au>
2020-06-01 12:30:48 +0530

    Add articles and pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,62 +1,57 @@
-
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Global SCSS Variables
-  */
+   ** Global SCSS Variables
+   */
   styleResources: {
-    scss: ['~/assets/styles/tokens.scss']
+    scss: ["~/assets/styles/tokens.scss"]
   },
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
-    '~/plugins/vue-placeholders.js',
-    '~/plugins/vue-observe-visibility.client.js'
+    "~/plugins/vue-placeholders.js",
+    "~/plugins/vue-observe-visibility.client.js"
   ],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    '@nuxtjs/svg',
-    '@nuxtjs/style-resources',
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ["@nuxtjs/svg", "@nuxtjs/style-resources"],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
+   ** Nuxt.js modules
+   */
+  modules: [],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
---
+++ pages/_username/_article.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+	export default {
+		name: '_article',
+	}
+</script>
+
+<style scoped>
+
+</style>
